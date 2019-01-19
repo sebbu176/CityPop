7704e7bfece01962d33a58548c737880cb935da0
Sebastian Bucht <sebbu176@student.liu.se>
2019-01-19 13:25:45 +0100

    Added so everything is not displayed at the same time

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
 import StartView from './components/StartView'
@@ -6,51 +7,116 @@ import axios from 'axios'
 import Search from './components/Search'
 import PopulationResult from './components/PopulationResult'
 import CountryResult from './components/CountryResult'
+import NoResult from './components/NoResult'
 class App extends Component {
   state = {
     results: [],
     result: [],
-    country: ''
+    country: '',
+    showSearchCity: true,
+    showSearchCountry: true,
+    showCountryResult: false,
+    showPopulationResult: false,
+    showNoResult: false
+  }
+  reset = () => {
+    this.setState({
+      results: [],
+      result: [],
+      country: '',
+      showSearchCity: true,
+      showSearchCountry: true,
+      showCountryResult: false,
+      showPopulationResult: false,
+      showNoResult: false
+    })
   }
 
   onclick = clickObject => {
     this.setState({ result: clickObject })
   }
-  
+
   handleSearchCity = inpuText => {
     axios
       .get(
         `http://api.geonames.org/searchJSON?name=${inpuText}&maxRows=1&username=weknowit&orderby=population&cities=cities1000`
       )
-      .then(res => this.setState({ result: res.data.geonames[0]}))
-      .catch(error => console.log(error))
+      .then(res => this.setState({ result: res.data.geonames[0], showSearchCity: false, showPopulationResult: true }))
+      .catch(error => this.setState({ showNoResult: true, showSearchCity: false }))
   }
 
-  
   handleSearchCountry = inpuText => {
     axios
       .get(`https://restcountries.eu/rest/v2/name/${inpuText}?fullText=true`)
-      .then(res => axios
-        .get(`http://api.geonames.org/searchJSON?country=${res.data[0].alpha2Code}&maxRows=3&username=weknowit&orderby=population&cities=cities1000`))
-        .then(res=> 
-          this.setState({
-            results: res.data.geonames,
-            country: res.data.geonames[0].countryName
-            
-          }))
-          .catch(error => console.log(error))
-      .catch(error => console.log(error))
+      .then(res =>
+        axios.get(
+          `http://api.geonames.org/searchJSON?country=${
+            res.data[0].alpha2Code
+          }&maxRows=3&username=weknowit&orderby=population&cities=cities1000`
+        )
+      )
+      .then(res =>
+        this.setState({
+          results: res.data.geonames,
+          country: res.data.geonames[0].countryName,
+          showSearchCountry: false,
+          showCountryResult: true
+        })
+      )
+      .catch(error => this.setState({ showNoResult: true, showSearchCountry: false }))
+      .catch(error => this.setState({ showNoResult: true, showSearchCountry: false }))
   }
+
   render() {
     return (
-      <div className="App">
-        <Header />
-        <StartView />
-        <Search onSubmit={this.handleSearchCountry} title={'Search by country'} placeholder={'Enter a country'} />
-        <Search onSubmit={this.handleSearchCity} title={'Search by city'} placeholder={'Enter a city'} />
-        <PopulationResult result={this.state.result} />
-        <CountryResult results={this.state.results} country={this.state.country} onclick={this.onclick} />
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header reset={this.reset} />
+            <div>
+              {this.state.showNoResult && <NoResult />}
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <React.Fragment>
+                    <StartView reset={this.reset} link1={'/searchbycity'} link2={'/searchbycountry'} />
+                  </React.Fragment>
+                )}
+              />
+              <Route
+                path="/searchbycountry"
+                render={props => (
+                  <React.Fragment>
+                    {this.state.showSearchCountry && (
+                      <Search
+                        onSubmit={this.handleSearchCountry}
+                        title={'Search by country'}
+                        placeholder={'Enter a country'}
+                      />
+                    )}
+                    {this.state.showCountryResult && (
+                      <CountryResult results={this.state.results} country={this.state.country} onclick={this.onclick} />
+                    )}
+                    {this.state.showPopulationResult && <PopulationResult result={this.state.result} />}
+                  </React.Fragment>
+                )}
+              />
+              <Route
+                path="/searchbycity"
+                render={props => (
+                  <React.Fragment>
+                    {this.state.showSearchCity && (
+                      <Search onSubmit={this.handleSearchCity} title={'Search by city'} placeholder={'Enter a city'} />
+                    )}
+                    {this.state.showPopulationResult && <PopulationResult result={this.state.result} />}
+                  </React.Fragment>
+                )}
+              />
+            </div>
+          </div>
+        </div>
+      </Router>
     )
   }
 }
