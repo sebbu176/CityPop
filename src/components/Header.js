4e387fb02e637828871dd6f07f4f061d536be4d5
Sebastian Bucht <sebbu176@student.liu.se>
2019-01-19 13:27:47 +0100

    added return onthe header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 
 export class Header extends Component {
 render() {
   return (
     <header style={headerStyle}>
-      
+      <Link style={linkStyle} onClick={this.props.reset.bind()} to="/">
       <h1>CityPop</h1>
-    
+      </Link>
     </header>
   )
 }}
@@ -16,6 +17,10 @@ const headerStyle = {
     color: '#333',
     textAlign: 'center',
     padding: '10px'
+  }
+  const linkStyle = {
+    color: '#333',
+    textDecoration: 'none'
   }
 
 export default Header
